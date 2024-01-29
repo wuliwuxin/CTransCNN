d4237a1b9af682939fc229f8b8aa31e433bc3d12
Xin WU <wu1351658806@163.com>
2024-01-29 15:58:25 +0800

    Update
    Update

--- model/apis/__init__.py
+++ model/apis/__init__.py
@@ -1,4 +1,3 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 from .inference import inference_model, init_model, show_result_pyplot
 from .test import multi_gpu_test, single_gpu_test
 from .train import init_random_seed, set_random_seed, train_model
